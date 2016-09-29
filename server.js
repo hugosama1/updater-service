2b0039315a4075d62028b37a5a9a73ce7f66430a
hugosama <hugosama1@gmail.com>
2016-09-28 22:47:16 -0600

    added message service

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 const path = require('path');
+const PORT = 8080;
+var fs = require('fs');
 
 var app = express();
+app.use(bodyParser.json());
 
 app.get('/version', function (req, res) {
   res.sendFile(path.join(__dirname, '/version.json'));
@@ -11,6 +15,36 @@ app.get('/samalinne.apk', function (req, res) {
 	res.sendFile(path.join(__dirname, '/samalinne.apk'));
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.post('/messages', function (req, res) {
+	var messages = getMessages();
+	var message = req.body.message;
+	if(message) {
+		var lastMessage = messages[messages.length-1];
+		var date = lastMessage.date + (1000 * 60 * 60 * 24);
+		var _id = lastMessage._id + 1;
+		messages.push({
+			_id: _id,
+			date :  date,
+			message : message
+		});
+	}
+	res.json({ insertado : true });
+});
+
+app.get('/messages', function (req, res) {
+	var messages = getMessages();
+	var date = req.query.date ? req.query.date : 0;
+	res.json(messages.filter( x =>  x.date > date));
+});
+
+function getMessages() {
+	return JSON.parse(fs.readFileSync('messages.json', 'utf8'));
+}
+
+
+app.get('/')
+
+
+app.listen(PORT, function () {
+  console.log('Listening on port '+PORT+'!');
 });
